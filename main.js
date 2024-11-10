83ec6483a23e1f22ebd78d16961511bff3779b19
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-11-10 17:15:51 +0100

    copy current unix timestamp, fix hour displaying incorrectly, animate copy

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const clock_interval = setInterval(function() 
 {
     var date = new Date(Date.now());
+    var start = "";
+    if (date.getMinutes() < 10)
+        {
+            mid = "0"
+        }
+    var mid = ":";
     var mid = ":";
     if (date.getMinutes() < 10)
     {
         mid = ":0"
     }
 
-    var time = date.getHours() + mid + date.getMinutes();
-    document.getElementById('clock').innerHTML = time;
+    var time = start + date.getHours() + mid + date.getMinutes();
+    document.getElementById("clock").innerHTML = time;
+    document.getElementById("timestamp").innerHTML = Math.round(Date.now() / 1000);
 }, 100);
+
+
+const unix = document.getElementById("timestamp");
+unix.addEventListener("click", function(){
+    navigator.clipboard.writeText(Math.round(Date.now() / 1000));
+    unix.classList.toggle("clicked");
+    setTimeout( function(){
+        unix.classList.toggle("clicked");
+    }, 1000);
+});
--- page_title.js
+++ page_title.js
@@ -1,10 +1,15 @@
 const title_interval = setInterval(function()
 {
     var date = new Date(Date.now());
+    var start = "";
+    if (date.getMinutes() < 10)
+        {
+            mid = "0"
+        }
     var mid = ":";
     if (date.getMinutes() < 10)
     {
         mid = ":0"
     }
-    document.title = date.getHours() + mid + date.getMinutes();
+    document.title = start + date.getHours() + mid + date.getMinutes();
 }, 1000);
